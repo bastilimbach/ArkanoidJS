fa143a6618b91515f6fe79e4b769075d0f138448
Sebastian Limbach <bastilimbach@gmail.com>
2017-10-19 19:34:48 +0200

    Disable no-extraneous-dependencies rule from weback config files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+/* eslint import/no-extraneous-dependencies: 0 */
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
