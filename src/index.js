b67633938d51888f50c581c6131b95a7de112bc3
Sebastian Limbach <bastilimbach@gmail.com>
2017-11-21 18:44:02 +0100

    Add app index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import './app.scss' // eslint-disable-line no-unused-vars
-import './index.html' // eslint-disable-line no-unused-vars
+import '../public/app.scss'
+import '../public/index.html'
+import Arkanoid from './Arkanoid'
 
-document.write('Hello World')
+document.addEventListener('DOMContentLoaded', () => {
+  window.requestAnimFrame = window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (callback) { window.setTimeout(callback, 1000 / 60) }
 
-const [a, , c] = [1, 2, 3]
-
-console.log(a, c)
+  const game = new Arkanoid()
+  function startGameLoop() {
+    game.update()
+    window.requestAnimFrame(startGameLoop)
+  }
+  startGameLoop()
+})
