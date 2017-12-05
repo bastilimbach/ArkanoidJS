6c4a60e65d83d0d6b56074f106830a7a7f737c31
Sebastian Limbach <bastilimbach@gmail.com>
2017-12-05 19:27:56 +0100

    Fix mouse position on transformed canvas

--- src/Controller/MouseController.js
+++ src/Controller/MouseController.js
@@ -2,12 +2,20 @@ export default class MouseController {
   constructor(game, canvas, controllableItems) {
     this.game = game
     this.items = controllableItems
+    this.canvas = canvas
     canvas.addEventListener('mousemove', this.mouseMoved.bind(this))
     canvas.addEventListener('click', this.mouseClicked.bind(this))
   }
 
   mouseMoved(event) {
-    const mousePosition = [event.layerX, event.layerY]
+    console.log(event)
+    const rect = this.canvas.getBoundingClientRect()
+    const scaleX = this.canvas.width / rect.width
+    const scaleY = this.canvas.height / rect.height
+    const mousePosition = [
+      (event.clientX - rect.left) * scaleX,
+      (event.clientX - rect.top) * scaleY,
+    ]
     for (let i = 0; i < this.items.length; i += 1) {
       this.items[i].move(mousePosition)
     }
